418537088116de82c7f6945f65039a0b71a13002
Lena Jankoschek <lena.jankoschek@live.de>
2025-05-08 11:03:42 +0200

    add some comments and clean up imoprts

--- baselines/EmbeddingDotProduct.py
+++ baselines/EmbeddingDotProduct.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from utils import *
 import copy
 
